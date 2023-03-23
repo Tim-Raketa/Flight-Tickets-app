447036f34be1e00af4c7a18c6a3164be8e092c1a
Dusan-Stanimirovic <93325150+Dusan-Stanimirovic@users.noreply.github.com>
2023-03-23 19:16:06 +0100

    changed int to Integer

--- src/main/java/com/example/demo/Model/Flight.java
+++ src/main/java/com/example/demo/Model/Flight.java
@@ -8,12 +8,12 @@ import java.sql.Date;
 @Document("flights")
 public class Flight {
     @Id
-    private int Id;
+    private Integer Id;
     private Date Begin;
     private Date End;
     private String StartingPlace;
     private String Destination;
-    private int MaxSeats;
-    private int TakenSeats;
+    private Integer MaxSeats;
+    private Integer TakenSeats;
     private float SeatPrice;
 }
