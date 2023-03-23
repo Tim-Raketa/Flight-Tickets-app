e40d0d6c2bad0abd3d246b9c03c377e978c864b5
Dusan-Stanimirovic <93325150+Dusan-Stanimirovic@users.noreply.github.com>
2023-03-23 19:37:46 +0100

    Connected Ticket class to db

--- src/main/java/com/example/demo/Model/Ticket.java
+++ src/main/java/com/example/demo/Model/Ticket.java
@@ -1,6 +1,12 @@
 package com.example.demo.Model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("tickets")
 public class Ticket {
+    @Id
+    private Integer Id;
     private Flight Flight;
     private User User;
     private Integer NumberOfSeats;
