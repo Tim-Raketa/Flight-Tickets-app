009fa39de04af50f8e6edda1edf6a58f8ca1d0a8
Dusan-Stanimirovic <93325150+Dusan-Stanimirovic@users.noreply.github.com>
2023-03-25 13:17:50 +0100

    added constructor, getters and setters

--- src/main/java/com/example/demo/Model/Flight.java
+++ src/main/java/com/example/demo/Model/Flight.java
@@ -16,4 +16,79 @@ public class Flight {
     private Integer MaxSeats;
     private Integer TakenSeats;
     private float SeatPrice;
+
+    public Flight(Integer id, Date begin, Date end, String startingPlace, String destination, Integer maxSeats, float seatPrice) {
+        Id = id;
+        Begin = begin;
+        End = end;
+        StartingPlace = startingPlace;
+        Destination = destination;
+        MaxSeats = maxSeats;
+        SeatPrice = seatPrice;
+        TakenSeats=0;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Date getBegin() {
+        return Begin;
+    }
+
+    public void setBegin(Date begin) {
+        Begin = begin;
+    }
+
+    public Date getEnd() {
+        return End;
+    }
+
+    public void setEnd(Date end) {
+        End = end;
+    }
+
+    public String getStartingPlace() {
+        return StartingPlace;
+    }
+
+    public void setStartingPlace(String startingPlace) {
+        StartingPlace = startingPlace;
+    }
+
+    public String getDestination() {
+        return Destination;
+    }
+
+    public void setDestination(String destination) {
+        Destination = destination;
+    }
+
+    public Integer getMaxSeats() {
+        return MaxSeats;
+    }
+
+    public void setMaxSeats(Integer maxSeats) {
+        MaxSeats = maxSeats;
+    }
+
+    public Integer getTakenSeats() {
+        return TakenSeats;
+    }
+
+    public void setTakenSeats(Integer takenSeats) {
+        TakenSeats = takenSeats;
+    }
+
+    public float getSeatPrice() {
+        return SeatPrice;
+    }
+
+    public void setSeatPrice(float seatPrice) {
+        SeatPrice = seatPrice;
+    }
 }
--- src/main/java/com/example/demo/Model/Ticket.java
+++ src/main/java/com/example/demo/Model/Ticket.java
@@ -10,4 +10,43 @@ public class Ticket {
     private Flight Flight;
     private User User;
     private Integer NumberOfSeats;
+
+    public Ticket(Integer id, com.example.demo.Model.Flight flight, com.example.demo.Model.User user, Integer numberOfSeats) {
+        Id = id;
+        Flight = flight;
+        User = user;
+        NumberOfSeats = numberOfSeats;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public com.example.demo.Model.Flight getFlight() {
+        return Flight;
+    }
+
+    public void setFlight(com.example.demo.Model.Flight flight) {
+        Flight = flight;
+    }
+
+    public com.example.demo.Model.User getUser() {
+        return User;
+    }
+
+    public void setUser(com.example.demo.Model.User user) {
+        User = user;
+    }
+
+    public Integer getNumberOfSeats() {
+        return NumberOfSeats;
+    }
+
+    public void setNumberOfSeats(Integer numberOfSeats) {
+        NumberOfSeats = numberOfSeats;
+    }
 }
