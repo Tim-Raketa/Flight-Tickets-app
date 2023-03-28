d5ddff899605f5bca8e3936561ae2f8b473d1376
IsidoraKnezevic <knezevic.ra47.2019@uns.ac.rs>
2023-03-28 21:11:16 +0200

    Implemented Flight CRUD and id generator

--- src/main/java/com/example/demo/Controllers/FlightController.java
+++ src/main/java/com/example/demo/Controllers/FlightController.java
@@ -4,24 +4,43 @@ import com.example.demo.DTO.FlightDTO;
 import com.example.demo.DTO.FlightSearchDTO;
 import com.example.demo.Model.Flight;
 import com.example.demo.Service.FlightService;
+import com.example.demo.Service.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.example.demo.Model.Flight.SEQUENCE_NAME;
+
 @RestController
 @RequestMapping(value = "/Flights", produces = MediaType.APPLICATION_JSON_VALUE)
 public class FlightController {
     @Autowired
     private FlightService flightService;
-    @GetMapping(value="/")
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+    @GetMapping(value="/getAll")
     public List<Flight> GetAll(){
         return flightService.GetAll();
     }
     @PostMapping(value="/search",consumes = "application/json")
     public List<FlightDTO> search(@RequestBody FlightSearchDTO details){
         return flightService.Search(details);
+    }
+
+    @PostMapping("/addFlight")
+    public String createFlight(@RequestBody Flight newFlight){
+        newFlight.setId(sequenceGeneratorService.getSequenceNumber(SEQUENCE_NAME));
+        flightService.createFlight(newFlight);
+        return "Added flight with id: " + newFlight.getId();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteFlight(@PathVariable Integer id){
+        flightService.deleteFlight(id);
+        return "Flight deleted ";
     }
 
 }
